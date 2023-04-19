7569789fc21a45311feb04c6d86bbeaba7eb1cdc
Orion-Winsor <111298704+Orion-Winsor@users.noreply.github.com>
2023-04-19 07:42:49 -0600

    added new package and classes, began adding frames: errors present

--- src/game/controller/Controller.java
+++ src/game/controller/Controller.java
@@ -1,14 +1,24 @@
 package game.controller;
 
+import javax.swing.JOptionPane;
+import game.view.GameFrame;
+
 public class Controller 
 {
+	private GameFrame frame;
+	
 	public Controller()
-	{
-		
+	{	
+		this.frame = new GameFrame(this);
 	}
 	
 	public void start()
 	{
 		
+	}
+	
+	public void handleError(Exception error)
+	{
+		JOptionPane.showMessageDialog(frame, error.getMessage(), "Ooops!", JOptionPane.ERROR_MESSAGE);
 	}
 }
