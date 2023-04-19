ff40cacc446e08c8368ce32041ef919b60041f06
Orion-Winsor <111298704+Orion-Winsor@users.noreply.github.com>
2023-04-19 08:47:33 -0600

    added, possibly, nessasary buffered image. revised imports due to typo. created new panel/frame

--- src/game/view/SpritePanel.java
+++ src/game/view/SpritePanel.java
@@ -3,6 +3,8 @@ package game.view;
 import game.controller.*;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.Dimension;
 
 import java.io.File;
 import java.io.IOException;
@@ -15,23 +17,31 @@ public class SpritePanel extends JPanel
 {
 	private Controller app;
 	private Color currentColor;
+	private BufferedImage currentCanvas;
+	private BufferedImage stamp;
 	
 	public SpritePanel(Controller app)
 	{
 		super();
 		
 		this.app = app;
+		this.currentCanvas = new BufferedImage(700, 700, BufferedImage.TYPE_INT_ARGB);
+		this.stamp = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
 		
+		setupPanel();
+		updateImage();
 	}
 	
 	public void setupPanel()
 	{
-		
+		this.setPreferredSize(new Dimension(700, 700));
+		this.setBackground(Color.black);
+		this.currentColor = Color.gray;
 	}
 	
 	private void updateImage()
 	{
-		
+		Graphics2D drawingGraphics = currentCanvas.createGraphics();
 
 	}
 }
